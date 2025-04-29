4a1d414a2bbb4872c5a0dddbc2e2951e44205cd1
shaniqua16 <git@github.com>
2025-04-29 13:07:22 -0500

    updated review errors

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ const mainRouter = express.Router();
 
 const authExports = require('./auth');
 const itemsRouter = require('./items');
-// const reviewsRouter = require('./reviews');
+const reviewsRouter = require('./reviews');
 
 mainRouter.use('/auth', authExports.router);
 mainRouter.use('/items', itemsRouter);
-// mainRouter.use('/reviews', reviewsRouter);
+mainRouter.use('/reviews', reviewsRouter);
 
 mainRouter.use((req, res, next) => {
     const error = new Error('Not found');
--- api/items.js
+++ api/items.js
@@ -1,16 +1,10 @@
 const {router} = require('../common');
 const {getAllItems, getIemsById} = require('./itemsController');
-const {getAllReviewsForItem, postReview, getReviewById} = require('./reviewsController');
-const {middleware} = require('./auth');
 
 
 router.get('/', getAllItems);
 router.get('/:itemsId', getIemsById);
-router.get('/:itemId/reviews', getAllReviewsForItem);
 
-
-router.get('/:itemId/reviews/:reviewId', getReviewById);
-router.post('/:itemId/reviews',middleware, postReview);
 
 
 module.exports = router;
